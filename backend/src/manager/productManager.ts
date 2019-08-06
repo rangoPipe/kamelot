25e7737943875a1aadc304d3dda00a36500caa9d
ideassimples <farango@ideassimples.com.co>
2019-08-05 22:46:47 -0500

    Se termina Manager de Product

--- backend/src/manager/productManager.ts
+++ backend/src/manager/productManager.ts
@@ -1,7 +1,6 @@
 import productModel, { Product } from "../model/product";
 import BaseException from "../common/baseException";
 import AppResponse from "../common/appResponse";
-const translation = require('../translation/es.json');
 
 class ProductManager {
 
@@ -9,23 +8,33 @@ class ProductManager {
 
     }
 
-    public allProducts() {
+    public async getAllProducts():Promise<AppResponse> {
         try {
-            let products:Product[] = [];
-
-            return products;
+            const products:Product[] = await productModel.find();
+            return new AppResponse(true, products);
             
         } catch (error) {
             new BaseException(500, error);
-            throw new AppResponse(false, )
+            throw new AppResponse(false, error );
         }    
+    }
+
+    public async getOne(model:Product):Promise<AppResponse>  {
+        try {
+            const product:Product|null =  await productModel.findOne(model.id);
+            return new AppResponse(true, product);
+
+        } catch (error) {
+            new BaseException(500, error);
+            throw new AppResponse(false, error );
+        }
     }
 
     public async save(product:Product):Promise<AppResponse> {
 
         try {
             let result = await productModel.findById(product.id);         
-            return (!result) ? await productController.saveProduct(product) : await productController.updateProduct(product);
+            return (!result) ? await productManager.saveProduct(product) : await productManager.updateProduct(product);
             
         } catch (error) {
             new BaseException(500, error); 
@@ -36,7 +45,7 @@ class ProductManager {
     private async saveProduct(product:Product):Promise<AppResponse> {
         try {
             const res = await product.save();
-            return new AppResponse(true, translation.common.save, res );
+            return new AppResponse(true, res );
         }
         catch(error){
             new BaseException(500, error);
@@ -48,12 +57,22 @@ class ProductManager {
         try {
             product.dateUpdate = new Date();
             const res = productModel.findByIdAndUpdate(product.id, product);
-            return new AppResponse(true, translation.common.save, res );
+            return new AppResponse(true, res );
         } catch (error) {
             new BaseException(500, error);
-            return new AppResponse(false, 'Error interno');
+            return new AppResponse(false, error);
+        }
+    }
+
+    private async disable(model:Product): Promise<AppResponse>  {
+        try {
+            const product:Product|null = await productModel.findByIdAndUpdate(model.id, { $set: {activo:false}});
+            return new AppResponse(true, product );
+        } catch (error) {
+            new BaseException(500, error);
+            return new AppResponse(false, error);
         }
     }
 }
 
-export const productController = new ProductController();
+export const productManager = new ProductManager();
