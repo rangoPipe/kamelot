4835715f13fe2388bc3546dc6f4874856cb1ec0b
ideassimples <farango@ideassimples.com.co>
2019-08-10 13:24:23 -0500

    Se mueven los modelos

---
+++ model/product.ts
@@ -0,0 +1,42 @@
+import mongoose, { mongo } from "mongoose";
+
+
+const schProduct = new mongoose.Schema ({
+    id: mongoose.Schema.Types.ObjectId,
+    idProveedor : mongoose.Schema.Types.ObjectId,
+    name : {
+        type : String,
+        required : true,
+        uppercase: true
+    },
+    ean  : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    costBuy: Number,
+    costSale: Number,
+    dateCreate : {
+        type : Date,
+        required: true
+    },
+    dateUpdate : Date,
+    active     : {
+        type : Boolean,
+        default : true
+    }
+});
+
+export interface Product extends mongoose.Document {
+    _id: mongoose.Schema.Types.ObjectId;
+    idProveedor : mongoose.Schema.Types.ObjectId;
+    name : String;
+    ean: String;
+    costBuy: Number;
+    costSale: Number;
+    dateCreate : Date;
+    dateUpdate : Date;
+    active     : Boolean;
+}
+
+export default mongoose.model<Product>('Producto', schProduct);
